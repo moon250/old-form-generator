c274de834ed7ccba11cf6ea3810917d7f2a157e5
moon250 <mooneupho@gmail.com>
2020-11-20 20:50:37 +0100

    fix: travis-ci

--- src/FormGenerator.php
+++ src/FormGenerator.php
@@ -93,7 +93,7 @@ class FormGenerator
         if (true === $html_structure) {
             $submitValue = $this->config->get('form_submit_value')
             ? $this->config->get('form_submit_value') : '';
-            $submit = $this->config->get('form_submit') && $html_structure ?
+            $submit = $this->config->get('form_submit') && $html_structure === true ?
                 "\n    <input type=\"submit\"{$submitValue}>" :
                 '';
             $id = $this->config->get('form_id') ? ' id="' . $this->config->get('form_id') . '"' : '';
